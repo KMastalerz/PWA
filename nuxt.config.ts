6ffec06d82c0243edf124916d498b07b207d67d0
Krzysztof Mastalerz <krzysztof.mastalerz@outlook.com>
2025-04-26 19:39:01 +0200

    PWA done

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,42 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: [
-    'bulma',
-    '~/assets/css/main.css',
-  ]
-})
+  css: ['bulma', '~/assets/css/main.css'],
+  devServer: {
+      https: {
+          key: './localhost-key.pem',
+          cert: './localhost.pem',
+      },
+  },
+  modules: ['@vite-pwa/nuxt'],
+  pwa: {
+      registerType: 'autoUpdate',
+      manifest: {
+          name: 'Nuxt 3 PWA 2025',
+          short_name: 'Nuxt3PWA',
+          description: 'Demo Nuxt 3 PWA',
+          theme_color: '#4DBA87',
+          background_color: '#ffffff',
+          display: 'standalone',
+          icons: [
+              {
+                  src: 'pwa-192.png',
+                  sizes: '192x192',
+                  type: 'image/png',
+              },
+              {
+                  src: 'pwa-512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+              }
+          ],
+      },
+      workbox: {
+          globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      },
+      devOptions: {
+          enabled: true,
+          type: 'module',
+      },
+  },
+});
